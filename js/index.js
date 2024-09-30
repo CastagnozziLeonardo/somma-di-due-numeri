347dfae97b4e038e484d815177a77812381c68df
lai2-24 <lai2-24@itismeucci.com>
2024-09-30 11:49:35 +0200

    second commit

--- js/index.js
+++ js/index.js
@@ -2,13 +2,11 @@ const NUMERI = document.querySelectorAll('.input');
 const BOTTONE = document.getElementById('bottone');
 const RISULTATO = document.getElementById('result');
 
-
 function esegui(){
     let somma = 0;
-    somma = Number(NUMERI[0].value) + Number(NUMERI[1].value);
+    somma = NUMERI[0].value + NUMERI[1].value;
     RISULTATO.innerHTML = somma;
 }
-
 
 BOTTONE.addEventListener('click', esegui);
 
